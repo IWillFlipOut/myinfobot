f62528113c01633daec51019df4f241739a43722
IWillFlipOut <abrussow3@gmail.com>
2023-06-07 14:01:52 +0000

    Development environment configuration

--- myinfobot/settings.py
+++ myinfobot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2fgqn0tk+_!w7ezj^(jf#pp&!5xela&#@=gc)hww0nnu%ae#5c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.33.10', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myinfobot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myinfobot',
+        'USER': 'postgres',
+        'PASSWORD': 'j*!Y~2*?r)ca+/L=\PP"',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -117,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/myinfobot/myinfobot/staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
